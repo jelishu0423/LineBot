f4ebbf7b0741358620356c762a20a8c200b82915
jelishu0423 <71770740+jelishu0423@users.noreply.github.com>
2022-01-18 23:01:09 +0800

    Update xcoding7.py

--- xcoding7.py
+++ xcoding7.py
@@ -43,10 +43,10 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = ImageSendMessage(image=event.message.image)
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
+    message = ImageSendMessage(original_content_url='https://example.com/original.jpg', preview_image_url='https://example.com/preview.jpg')
     line_bot_api.reply_message(event.reply_token, message)
-    #message = TextSendMessage(text=event.message.text)
-    #line_bot_api.reply_message(event.reply_token,message)
 
 #主程式
 import os
