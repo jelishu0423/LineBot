96c8da1edea15bd3839ef62fb0e5ba4b642f0361
jelishu0423 <71770740+jelishu0423@users.noreply.github.com>
2022-01-18 23:08:13 +0800

    Update xcoding7.py

--- xcoding7.py
+++ xcoding7.py
@@ -44,9 +44,14 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
-    message = ImageSendMessage(original_content_url='https://example.com/original.jpg', preview_image_url='https://example.com/preview.jpg')
-    line_bot_api.reply_message(event.reply_token, message)
+    if re.match('告訴我秘密',message):
+        image_message = ImageSendMessage(
+            original_content_url='https://images.builderservices.io/s/cdn/v1.0/i/m?url=https%3A%2F%2Fstorage.googleapis.com%2Fproduction-bluehost-v1-0-9%2F659%2F790659%2FAtmP8Pmy%2F9c8c1e647eb14e01898043c0c60bf03a&methods=resize%2C1000%2C5000',
+            preview_image_url='https://images.builderservices.io/s/cdn/v1.0/i/m?url=https%3A%2F%2Fstorage.googleapis.com%2Fproduction-bluehost-v1-0-9%2F659%2F790659%2FAtmP8Pmy%2Ffd2258c5ea6c43f591e8d9930d152b94&methods=resize%2C1000%2C5000'
+        )
+        line_bot_api.reply_message(event.reply_token, image_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 
 #主程式
 import os
